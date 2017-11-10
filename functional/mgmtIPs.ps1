4851ab2bef2e778b713496520d1afd6e6447235d
Daniel Weissenborn <dweissenborn@outlook.com>
2017-11-10 12:41:59 -0800

    DW updates
    (wip)

---
+++ functional/mgmtIPs.ps1
@@ -0,0 +1,7 @@
+﻿$secpasswd = ConvertTo-SecureString "" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ('', $secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant 72f988bf-86f1-41af-91ab-2d7cd011db47 -Credential $mycreds
+##
+$pubIPs = Get-AzureRmPublicIpAddress -ResourceGroupName 'existingTempalte_api'
+$mgmtpubIPs = $pubIPs | ?{$_.name -like 'mgmt*'}
+$mgmtpubIPs.IPAddress
