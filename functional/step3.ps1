71a69f406a79dc9af0c5654216c15c0b41863558
Daniel Weissenborn <dweissenborn@outlook.com>
2017-11-10 12:57:48 -0800

    DW updates

---
+++ functional/step3.ps1
@@ -0,0 +1,13 @@
+﻿
+$array = 40,50
+$average = $array | measure -Average
+if ($average.Average -le 40){
+$VMs = Get-AzureRmVM -ResourceGroupName 'existingtempalte_api' -Status | ?{$_.name -like 'VM-Series*'}
+$onVMs = $VMs | ?{$_.PowerState -like '*running*'}
+$onVMs[0] | Stop-AzureRmVM -Force
+}
+if ($average.Average -ge 60){
+$VMs = Get-AzureRmVM -ResourceGroupName 'existingtempalte_api' -Status | ?{$_.name -like 'VM-Series*'}
+$offVMs = $VMs | ?{$_.PowerState -like '*deallocated*'}
+$offVMs[0] | Start-AzureRmVM
+}
